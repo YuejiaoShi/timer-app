b2bd11643602b184ffb129db90820f835d287c07
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 17:30:22 +0200

    use parameters in return

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,7 +4,8 @@ import Container from "./UI/Container.tsx";
 export default function Timer({ name, duration }: TimerProps) {
   return (
     <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
+      <h2>{name}</h2>
+      <p>{duration}</p>
     </Container>
   );
 }
