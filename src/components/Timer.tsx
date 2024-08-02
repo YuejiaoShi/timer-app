a31b0387714f68417fd1a4f6d6a5b3fe2c0f5a69
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 17:29:21 +0200

    import Timer type and give it a new name to avoid conflict

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,7 @@
-import Container from './UI/Container.tsx';
+import { type Timer as TimerProps } from "../store/timer-context.tsx";
+import Container from "./UI/Container.tsx";
 
-export default function Timer() {
+export default function Timer({ name, duration }: TimerProps) {
   return (
     <Container as="article">
       <h2>TODO: TIMER NAME</h2>
