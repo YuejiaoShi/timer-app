9f918c233da3c0cf4e3ce84ddefece1773aec594
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 16:36:45 +0200

    add a guard to double check the timersContext not null

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,9 @@ import { TimersContext } from "../store/timer-context.tsx";
 
 export default function Header() {
   const timersContext = useContext(TimersContext)!;
+  if(timersContext === null){
+    throw new Error('TimersContext is undefined. Make sure you are using it within a TimersProvider.')
+  }
 
   return (
     <header>
